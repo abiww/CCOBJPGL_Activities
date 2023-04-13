2c83b9fe68c10aa59b78eecfba3bb303ea3d72b1
Allado, Abigail <110992588+abiww@users.noreply.github.com>
2023-04-13 15:39:46 +0800

    Update BookRental.java

--- Book Rental System/BookRental.java
+++ Book Rental System/BookRental.java
@@ -1,82 +1,84 @@
+package ccobjpgl_activities;
+
 import java.util.Scanner;
 
 public class BookRental {
-	String bookTitle;
-	String authorName;
-	int numberOfCopies;
+    String bookTitle;
+    String authorName;
+    int numberOfCopies;
 	
-	public BookRental (String bookTitle, String authorName, int numberOfCopies) {
-		this.bookTitle = bookTitle;
-		this.authorName = authorName;
-		this.numberOfCopies = numberOfCopies;
-	}
+    public BookRental (String bookTitle, String authorName, int numberOfCopies) {
+	this.bookTitle = bookTitle;
+	this.authorName = authorName;
+        this.numberOfCopies = numberOfCopies;
+    }
 	
-	public String getBookTitle() {
+    public String getBookTitle() {
         return bookTitle;
     }
 	
-	public String getAuthorName() {
+    public String getAuthorName() {
         return authorName;
     }
 	
-	public int getNumberOfCopies() {
+    public int getNumberOfCopies() {
         return numberOfCopies;
     }
 	
-	public void rentedBook() {
-		numberOfCopies--;
-	}
+    public void rentedBook() {
+	numberOfCopies--;
+    }
 	
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 		
-		System.out.println("ELECTRONIC BOOK RENTAL SYSTEM");
-		System.out.println("*****************************");
+        System.out.println("ELECTRONIC BOOK RENTAL SYSTEM");
+	System.out.println("*****************************");
 		
-		BookRental bookRental[] = new BookRental[3];
-		//Setting rental books information
-		bookRental[0] = new BookRental("System Analysis and Design", "Gary B. Shelly", 2);
-		bookRental[1] = new BookRental("Android Application", "Corinne Hoisington", 3);	
-		bookRental[2] = new BookRental("Programming Concepts and Logic Formulation", "Rasauro E. Manuel", 4);
+	BookRental bookRental[] = new BookRental[3];
+	//Setting rental books information
+	bookRental[0] = new BookRental("System Analysis and Design", "Gary B. Shelly", 2);
+	bookRental[1] = new BookRental("Android Application", "Corinne Hoisington", 3);	
+	bookRental[2] = new BookRental("Programming Concepts and Logic Formulation", "Rasauro E. Manuel", 4);
 		
-		for (int i = 0; i < bookRental.length; i++) {
-			//Display books available for rent
-			System.out.println((i) + " " + bookRental[i].getBookTitle() + ", " + bookRental[i].getAuthorName());
-		}
+	for (int i = 0; i < bookRental.length; i++) {
+            //Display books available for rent
+            System.out.println((i) + " " + bookRental[i].getBookTitle() + ", " + bookRental[i].getAuthorName());
+	}
 		
-		System.out.println("*****************************");
+	System.out.println("*****************************");
 		
-		Scanner scan = new Scanner(System.in);
-		boolean continueRenting = true;
+	Scanner scan = new Scanner(System.in);
+	boolean continueRenting = true;
 		
-		while (continueRenting) {
-			System.out.print("CHOOSE A NUMBER TO RENT YOUR FAVORITE BOOK: ");
-			int choice = scan.nextInt();
+	while (continueRenting) {
+            System.out.print("CHOOSE A NUMBER TO RENT YOUR FAVORITE BOOK: ");
+            int choice = scan.nextInt();
 			
-			try {
-				BookRental selectedBook = bookRental[choice];
+            try {
+                BookRental selectedBook = bookRental[choice];		
+                
+                if (selectedBook.getNumberOfCopies() == 0) {
+                    System.out.println("No copies available");
+                } 
 				
-				if (selectedBook.getNumberOfCopies() == 0) {
-					System.out.println("No copies available");
-				} 
+		else {
+                    selectedBook.rentedBook();
+                    System.out.println("You Rented " + bookRental[choice].getBookTitle());
+                    System.out.println("Available Copies: " + bookRental[choice].getNumberOfCopies());
+		}
 				
-				else {
-					selectedBook.rentedBook();
-					System.out.println("You Rented " + bookRental[choice].getBookTitle());
-					System.out.println("Available Copies: " + bookRental[choice].getNumberOfCopies());
-				}
+		//To continue or end the program
+		System.out.print("Rent Again? Y/N: ");
+		String input = scan.next();
 				
-				//To continue or end the program
-				System.out.print("Rent Again? Y/N: ");
-				String input = scan.next();
-				
-				if (!input.equalsIgnoreCase("Y")) {
+		if (!input.equalsIgnoreCase("Y")) {
                     continueRenting = false;
                     System.out.print("Program End");
-				}
+		}
 				
-			} catch (IndexOutOfBoundsException e) {
-				 System.out.println("INDEX DOES NOT EXIST, Try Again!");
-			}
-		}	
-	}
+            } catch (IndexOutOfBoundsException e) {
+                System.out.println("INDEX DOES NOT EXIST, Try Again!");
+            }
+        }	
+    }
 }
